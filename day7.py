19fc2cd78c1954933ac2459d324811d9e0039414
xhain <pantha0@googlemail.com>
2020-12-07 23:41:23 +0100

    Adds day 7 p1 & p2 assertion + solution.

--- day7.py
+++ day7.py
@@ -13,7 +13,7 @@ Created on Mon Dec  7 22:08:52 2020
 
 data = [line.rstrip() for line in list(open("data/input7.txt", "r"))]
 
-test, test_ans = [
+test1, test_ans1 = [
         "light red bags contain 1 bright white bag, 2 muted yellow bags.",
         "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
         "bright white bags contain 1 shiny gold bag.",
@@ -25,9 +25,20 @@ test, test_ans = [
         "dotted black bags contain no other bags."
         ], 4
 
+test2, test_ans2 = [
+    "shiny gold bags contain 2 dark red bags.",
+    "dark red bags contain 2 dark orange bags.",
+    "dark orange bags contain 2 dark yellow bags.",
+    "dark yellow bags contain 2 dark green bags.",
+    "dark green bags contain 2 dark blue bags.",
+    "dark blue bags contain 2 dark violet bags.",
+    "dark violet bags contain no other bags."
+    ], 126
+
+
 # Part 1 Solution
-def get_bags(color):
-    lines = [line for line in data if color in line and line.index(color) != 0]
+def get_bags(color, rules):
+    lines = [line for line in rules if color in line and line.index(color) != 0]
     
     colorsTracker = []
     
@@ -40,7 +51,7 @@ def get_bags(color):
         
         for color in colors:
             colorsTracker.append(color)
-            bags = get_bags(color)
+            bags = get_bags(color, rules)
             colorsTracker += bags
         
         uniqueColors = []
@@ -50,6 +61,28 @@ def get_bags(color):
                 
     return uniqueColors
     
-print("/ part 1 : ", len(get_bags('shiny gold')))
+
+# Part 2 Solution
+def get_count(color, rules):
+    rule = ''.join(line for line in rules if line[:line.index(' bags')] == color)
     
+    if 'no' in rule:
+        return 1
     
+    rule = rule[rule.index('contain ')+len('contain '):].split()
+    ans, i = 0, 0
+    while i < len(rule):
+        count = int(rule[i])
+        color = rule[i+1] +' '+ rule[i+2]
+        ans += count * get_count(color, rules)
+        i += 4
+    return ans + 1
+
+
+# Results
+assert len(get_bags('shiny gold', test1)) == test_ans1
+assert get_count('shiny gold', test2)-1 == test_ans2
+print("/ part 1: ", len(get_bags('shiny gold', data)))
+print("/ part 2: ", get_count('shiny gold', data)-1)
+
+# That took too long. I need to practice recursions.
