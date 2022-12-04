059282b7a75e28808934d8271f644c77dd7d5320
xhain <pantha0@googlemail.com>
2022-12-04 15:12:45 +0100

    Adds Day4/Part1 solution.

--- 2022/day04.py
+++ 2022/day04.py
@@ -3,17 +3,23 @@
 def load_data(path):
     return list( open(path, "r") )
 
-# Day 2 / Part 1
+# Day 4 / Part 1
 data = load_data("data/day04.txt")
 
-chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 result = 0
-
+for line in data:
+    a, b = line.split(',')
+    a1, a2 = map(int, a.split('-'))
+    b1, b2 = map(int, b.split('-'))
+    if (a1 >= b1) and (a2 <= b2):
+        result += 1
+    elif (a1 <= b1) and (a2 >= b2):
+        result += 1
 
 print(f">> Day 4 / Part 1 Solution: {result}")
 
 
-# Day 2 / Part 2
+# Day 4 / Part 2
 result = 0
 
 print(f">> Day 4 / Part 2 Solution: {result}")
