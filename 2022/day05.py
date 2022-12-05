fb2843405c67e56836eda855154adecad38194ac
Maximilian Weber <maxweber@fb.com>
2022-12-05 12:11:21 +0100

    Adds Day 5 / Part 2 solution.

--- 2022/day05.py
+++ 2022/day05.py
@@ -7,23 +7,26 @@ def load_data(path):
 data = load_data("data/day05.txt")
 
 
-# get stack data from file
-initial_stack = data[:8]
+# get initial stack data from file
 stacks = {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: []}
-for line in initial_stack:
+for line in data[:8]:
     idx, stack_id = 0, 0
     line = line.strip('\n')
     for char in line:
         if idx%4 == 1:
             stack_id = stack_id%9
             if char != " ":
-                stacks[stack_id+1].append(char)
+                stacks[stack_id + 1].append(char)
             stack_id += 1
         idx += 1
 
 # correct order in stacks
 for idx in range(len(stacks)):
-    stacks[idx+1].reverse()
+    stacks[idx + 1].reverse()
+
+# save an original copy
+from copy import deepcopy
+init_stacks = deepcopy(stacks)
 
 # run commands
 for line in data[10:]:
@@ -31,13 +34,19 @@ for line in data[10:]:
     for i in range(int(num)):
         stacks[int(destination)].append(stacks[int(origin)].pop())
 
+# print solution (top item on each stack)
 print(">> Day 4 / Part 1 Solution: ")
 for stack in stacks:
     print(stacks[stack].pop())
 
 
-
 # Day 5 / Part 2
-result = 0
+stacks = deepcopy(init_stacks)
+for line in data[10:]:
+    _, num, _, origin, _, destination = line.strip('\n').split(' ')
+    stacks[int(destination)].extend(stacks[int(origin)][-int(num):])
+    stacks[int(origin)] = stacks[int(origin)][:-int(num)]
 
-print(f">> Day 4 / Part 2 Solution: {result}")
+print(">> Day 4 / Part 2 Solution: ")
+for stack in stacks:
+    print(stacks[stack].pop())
