051f789fa8ba5dfb9575ead3d70fef1991deeeb7
Maximilian Weber <maxweber@fb.com>
2022-12-09 11:22:07 +0100

    Adds day9 data - no solution yet.

---
+++ 2022/day09.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/asicpy
+# -*- coding: utf-8 -*-
+
+def load_data(path):
+    return list( open(path, "r") )
+
+# Day 9 / Part 1
+raw_data = load_data("data/day09.txt")
+result = 0
+
+
+print(f">> Day 6 / Part 1 Solution: {result}")
+
+
+# Day 9 / Part 2
+
+print(f">> Day 6 / Part 1 Solution: {result}")
+
